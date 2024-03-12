a122336d845b69e17945045aab659c72dba11019
1ceHat <artemdudorov2015@gmail.com>
2024-03-12 17:59:40 +0500

    Домашняя работа по теме "Различие атрибутов класса и экземпляра"

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 class Building:
+    total = 0
     def __init__(self, numFloor=None, type=None):
-        self.numberOfFloors = 0
+        Building.total += 1
+        self.numberOfFloors = 1
         self.buildType = "House"
         if numFloor: self.numberOfFloors = numFloor
         if type: self.buildType = type
@@ -8,10 +10,10 @@ class Building:
     def __eq__(self, other):
         return (self.numberOfFloors == other.numberOfFloors) and (self.buildType == other.buildType)
 
-my_house = Building(numFloor=9)
-second_house = Building(type="Hospital", numFloor=3)
-print(f"Я первая постройка! Мой тип {my_house.buildType}. Я {my_house.numberOfFloors}-этажная постройка.")
-print(f"Я вторая постройка! Мой тип {second_house.buildType}. Я {second_house.numberOfFloors}-этажная постройка.")
-print(my_house == second_house)
+for i in range(40):
+    new_build = Building()
+    print(f"Я {i+1}-ая постройка! Мой тип {new_build.buildType}. Я {new_build.numberOfFloors}-этажная постройка.")
+
+print(Building.total)
 
 
