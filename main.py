7984bad9bbee0c12d274b3d29a5bc27e580b9b3e
1ceHat <artemdudorov2015@gmail.com>
2024-03-14 23:42:02 +0500

    Домашняя работа по теме "Множественное наследование"

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
+class Vehicle:
+    def __init__(self):
+        super().__init__()
+        self.vehicle_type = None
+
+
 class Car:
-    horsePow = 0
-    def __init__(self, horse_power=None):
+    horse_power = 100
+
+    def __init__(self):
+        super().__init__()
         self.price = 1000000
-        if horse_power:
-            Car.horsePow = horse_power
 
     def horse_powers(self):
-        return self.horsePow
+        return Car.horse_power
 
 
-class Nissan(Car):
+class Nissan(Car, Vehicle):
 
     def __init__(self):
+        super().__init__()
         self.price = 2500000
+        self.vehicle_type = "car"
 
-    def horse_powers(self, horse_power):
-        self.horsePow = horse_power
-        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
-                                                   self.horsePow))
-
-
-class Kia(Car):
-    def __init__(self):
-        self.price = 1700000
-
-    def horse_powers(self, multip):
-        self.horsePow = int(self.horsePow * multip)
-        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
-                                                   self.horsePow))
+    def horse_powers(self):
+        self.horse_power = 200
+        print("{}'s horse power is {}".format(
+            self.__class__.__name__, self.horse_power
+        ))
 
 
-car_prototype = Car(horse_power=100)
 nissan_gtr = Nissan()
-kia_rio = Kia()
-
-print("Car prototype's horse powers is", car_prototype.horse_powers())
-nissan_gtr.horse_powers(horse_power=200)
-kia_rio.horse_powers(multip=1.5)
+print("Nissan GTR's type is {} and price is {}".format(
+    nissan_gtr.vehicle_type, nissan_gtr.price)
+)
+nissan_gtr.horse_powers()
