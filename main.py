ea20c11b898e8b7f329b97c8245b6e9ff31e567c
1ceHat <artemdudorov2015@gmail.com>
2024-03-14 22:36:30 +0500

    Домашняя работа по теме "Наследование классов"

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-class Building:
-    total = 0
-    def __init__(self, numFloor=None, type=None):
-        Building.total += 1
-        self.numberOfFloors = 1
-        self.buildType = "House"
-        if numFloor: self.numberOfFloors = numFloor
-        if type: self.buildType = type
+class Car:
+    horsePow = 0
+    def __init__(self, horse_power=None):
+        self.price = 1000000
+        if horse_power:
+            Car.horsePow = horse_power
 
-    def __eq__(self, other):
-        return (self.numberOfFloors == other.numberOfFloors) and (self.buildType == other.buildType)
-
-for i in range(40):
-    new_build = Building()
-    print(f"Я {i+1}-ая постройка! Мой тип {new_build.buildType}. Я {new_build.numberOfFloors}-этажная постройка.")
-
-print(Building.total)
+    def horse_powers(self):
+        return self.horsePow
 
 
+class Nissan(Car):
+
+    def __init__(self):
+        self.price = 2500000
+
+    def horse_powers(self, horse_power):
+        self.horsePow = horse_power
+        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
+                                                   self.horsePow))
+
+
+class Kia(Car):
+    def __init__(self):
+        self.price = 1700000
+
+    def horse_powers(self, multip):
+        self.horsePow = int(self.horsePow * multip)
+        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
+                                                   self.horsePow))
+
+
+car_prototype = Car(horse_power=100)
+nissan_gtr = Nissan()
+kia_rio = Kia()
+
+print("Car prototype's horse powers is", car_prototype.horse_powers())
+nissan_gtr.horse_powers(horse_power=200)
+kia_rio.horse_powers(multip=1.5)
