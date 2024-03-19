592b2ccd3e8e31f4a4738204370040349347a46b
1ceHat <artemdudorov2015@gmail.com>
2024-03-19 22:15:11 +0500

    Домашняя работа по теме "Режимы открытия файлов"

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-class Vehicle:
-    def __init__(self):
-        super().__init__()
-        self.vehicle_type = None
+file = open("Test.txt", mode="r", encoding="utf8")
+file_cont = file.read()
+file.close()
 
-
-class Car:
-    horse_power = 100
-
-    def __init__(self):
-        super().__init__()
-        self.price = 1000000
-
-    def horse_powers(self):
-        return Car.horse_power
-
-
-class Nissan(Car, Vehicle):
-
-    def __init__(self):
-        super().__init__()
-        self.price = 2500000
-        self.vehicle_type = "car"
-
-    def horse_powers(self):
-        self.horse_power = 200
-        print("{}'s horse power is {}".format(
-            self.__class__.__name__, self.horse_power
-        ))
-
-
-nissan_gtr = Nissan()
-print("Nissan GTR's type is {} and price is {}".format(
-    nissan_gtr.vehicle_type, nissan_gtr.price)
-)
-nissan_gtr.horse_powers()
+print(file_cont)
