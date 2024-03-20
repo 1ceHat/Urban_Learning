de9266003fcc569111f6cc4ea1d64ef0aa3ecace
1ceHat <artemdudorov2015@gmail.com>
2024-03-20 13:42:31 +0500

    Домашняя работа по теме "Файлы в ОС"

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-number_first_team = 20
-number_second_team = 15
-print("В команде \"Мастера кода\" всего %d участников."%number_first_team,
-      "В команде \"Волшебники данных\" всего %d участников."%number_second_team,
-      "Итого сегодня в схватке сойдутся %d человек!\n"%(number_first_team+number_second_team),
-      sep="\n")
+import os
+import time
 
-score_1 = 30
-score_2 = 34
-time_1 = 30
-time_2 = 20
-print("Первая команда решила {} задач, а вторая - {};".format(score_1, score_2))
+cur_dir = os.path.dirname(__file__)
+dirs = os.walk(cur_dir)
+for dirpath, dirname, files in dirs:
+    print("{dirpath:=^50}".format(dirpath=dirpath))
+    if not dirname: dirname = None
+    if not files: files = None
+    print(f"|-dirs-{dirname}\n|-files-{files}\n")
 
-print(f"Первая команда управилась за {time_1} минут, а вторая за {time_2};\n")
-if score_1==score_2:
-    print(f"Победила дружба! Обеими командами было решено по {score_1} задач!\n"
-          f"Скорость первой команды: {score_1/time_1}\n"
-          f"Скорость второй команды: {score_2/time_2}\n")
-elif score_1>score_2:
-    print(f"Победили \"Мастера кода\"!\n"
-          f"Они решили {score_1} задач за {time_1}, таким образом их скорость : {score_1/time_1}")
-else:
-    print(f"Победили \"Волшебники данных\"!\n"
-          f"Они решили {score_2} задач за {time_2}, таким образом их скорость : {score_2 / time_2}")
+    if files:
+        for file in files:
+            full_path = os.path.join(dirpath, file)
+            raw_last_write = os.path.getmtime(full_path)
+            size = os.path.getsize(full_path)
+            last_write = time.gmtime(raw_last_write)
+            print(f"{full_path}\n"
+                  f"|:| LAST WRITE: {last_write.tm_mday}-{last_write.tm_mon}-{last_write.tm_year}\n"
+                  f"|:| SIZE: {size} Bytes\n"
+                  f"|:| PARENT DIR: {os.path.dirname(full_path)}")
+    print("\n")
