6e4e7b6fb6049c9d82654c44efd6b4f722f44cab
1ceHat <artemdudorov2015@gmail.com>
2024-05-03 12:08:45 +0500

    Домашняя работа по теме "Доп задание по модулю "Базовые структуры данных""

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-class Car:
-    horsePow = 0
-    def __init__(self, horse_power=None):
-        self.price = 1000000
-        if horse_power:
-            Car.horsePow = horse_power
+import random as rnd
 
-    def horse_powers(self):
-        return self.horsePow
+students = {'John', 'Doe', 'Sonic', 'Star', 'Uaou', 'Aaaa'}
+sorted_students = sorted(tuple(students))
+grades = []
+average_marks = {}
 
-
-class Nissan(Car):
-
-    def __init__(self):
-        self.price = 2500000
-
-    def horse_powers(self, horse_power):
-        self.horsePow = horse_power
-        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
-                                                   self.horsePow))
-        return self.horsePow
+for student in sorted_students:
+    grades.append([])
+    for i in range(rnd.randint(2,10)):
+        grades[-1].append(rnd.randint(2,5))
+    print(grades[-1])
+    average_marks[student] = round(sum(grades[-1])/len(grades[-1]), 2)
 
 
-class Kia(Car):
-    def __init__(self):
-        self.price = 1700000
-
-    def horse_powers(self, multip):
-        self.horsePow = int(self.horsePow * multip)
-        print("Now {}'s horse powers is {}".format(self.__class__.__name__,
-                                                   self.horsePow))
-        return self.horsePow
-
-
-car_prototype = Car(horse_power=100)
-nissan_gtr = Nissan()
-kia_rio = Kia()
-
-print("Car prototype's horse powers is", car_prototype.horse_powers())
-nissan_gtr.horse_powers(horse_power=200)
-kia_rio.horse_powers(multip=1.5)
+for key, value in average_marks.items():
+    print(f"{key}'s average mark is {value}")
