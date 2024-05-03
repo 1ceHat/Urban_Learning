6ad82151a59be3e3e35109e3f29f3b345e3dda12
1ceHat <artemdudorov2015@gmail.com>
2024-05-03 13:11:39 +0500

    Домашняя работа по теме "Доп задание по модулю "Основные операторы""

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import random as rnd
 
-students = {'John', 'Doe', 'Sonic', 'Star', 'Uaou', 'Aaaa'}
-sorted_students = sorted(tuple(students))
-grades = []
-average_marks = {}
-
-for student in sorted_students:
-    grades.append([])
-    for i in range(rnd.randint(2,10)):
-        grades[-1].append(rnd.randint(2,5))
-    print(grades[-1])
-    average_marks[student] = round(sum(grades[-1])/len(grades[-1]), 2)
+number = rnd.randint(3, 20)
+password = ''
+deliters = set()
+for i in range(1, int(number**0.5)+1):
+    if number%i==0:
+        deliters.add(i)
+        deliters.add(number//i)
+sorted(deliters)
 
 
-for key, value in average_marks.items():
-    print(f"{key}'s average mark is {value}")
+for num in range(1,number//2+1):
+    for deliter in deliters:
+        if num<deliter-num:
+            password += str(num)+str(deliter-num)
+
+print(number, password)
